63eaf9a7c639cc4c467a286b5d344b797cdbf485
AnaisSalvador <anaissalvadorrodriguez@gmail.com>
2023-09-13 19:51:44 -0500

    Initial commit

---
+++ src/main/java/com/example/lab5_20202152/Lab520202152Application.java
@@ -0,0 +1,13 @@
+package com.example.lab5_20202152;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab520202152Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab520202152Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab5_20202152/Lab520202152ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5_20202152;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab520202152ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
